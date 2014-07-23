c29a413e44d49afca4b9b4df93422255e135bb14
Daniel Newton <djpnewton@gmail.com>
2014-07-24 11:20:35 +1200

    add script to more easily configure for android/arm

---
+++ configure_android.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+NDK_TOOLCHAIN=/opt/android-ndk-r9b/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64
+PREFIX=${NDK_TOOLCHAIN}/bin/arm-linux-androideabi-
+SYSROOT=/opt/android-ndk-r9b/platforms/android-9/arch-arm
+
+autoreconf -i
+
+./configure CC=${PREFIX}gcc CPPFLAGS="--sysroot=${SYSROOT}" CFLAGS="-march=armv7-a -mthumb -mfpu=vfp -mfloat-abi=softfp -nostdlib --sysroot=${SYSROOT}" LDFLAGS="--sysroot=${SYSROOT}" -host=arm-linux-androideabi --prefix=/tmp/libunwind_inst LIBS="-lc -lgcc"
+
+
