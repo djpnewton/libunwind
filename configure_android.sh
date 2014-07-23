ad7fe9343993174600dc13aded3b2fce751b8612
Daniel Newton <djpnewton@gmail.com>
2014-07-24 11:33:08 +1200

    use dl_iterate_phdr() modifications from https://github.com/ehsan/libunwind

--- configure_android.sh
+++ configure_android.sh
@@ -8,6 +8,6 @@ SYSROOT=/opt/android-ndk-r9b/platforms/android-9/arch-arm
 
 autoreconf -i
 
-./configure CC=${PREFIX}gcc CPPFLAGS="--sysroot=${SYSROOT}" CFLAGS="-march=armv7-a -mthumb -mfpu=vfp -mfloat-abi=softfp -nostdlib --sysroot=${SYSROOT}" LDFLAGS="--sysroot=${SYSROOT}" -host=arm-linux-androideabi --prefix=/tmp/libunwind_inst LIBS="-lc -lgcc"
+./configure CC=${PREFIX}gcc CPPFLAGS="--sysroot=${SYSROOT}" CFLAGS="-march=armv7-a -mthumb -mfpu=vfp -mfloat-abi=softfp --sysroot=${SYSROOT}" LDFLAGS="--sysroot=${SYSROOT}" -host=arm-linux-androideabi --prefix=/tmp/libunwind_inst LIBS="-lc -lgcc"
 
 
